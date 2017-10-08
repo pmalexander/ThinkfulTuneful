fbb130195f489c80ce299f2b50d4e3cf4fe9099b
Patrick Alexander Jr <pmalexander5@gmail.com>
2017-10-08 16:58:36 +0000

    adding to api.py

--- tuneful/api.py
+++ tuneful/api.py
@@ -11,13 +11,34 @@ from . import app
 from .database import session
 from .utils import upload_path
 
-@app.route("/api/list", methods=["GET"])
+@app.route("/api/music", methods=["GET"])
 @decorators.accept("application/json")
 def music_get():
     """Pulls playlist of music"""
     music = session.query(models.music).all()
-{
-    "file": {
-        "id": 7
-    }
-}
+    
+    """Converts data to JSON and returns result"""
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/music", methods=["POST"])
+@decorators.accept("application/json")
+def music_post():
+    """Adds music to playlist"""
+    music = session.query(models.music).get(id)
+    
+@app.route("/api/music/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+#    music = session.query()
+
+@app.route("/music/<int:id>", methods=["GET"])
+def unique_id(id):
+    entry = session.query(Entry).filter(Entry.id == id).first()
+    return render_template("music.html", entry=entry
+    )
+
+#{
+#    "file": {
+#        "id": 7
+#    }
+#}
